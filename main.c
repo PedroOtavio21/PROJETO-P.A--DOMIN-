cab92e7c074bf6c70b22e350c2981c03f12d09d8
PedroOtavio21 <pedrootavio.bezerra14@gmail.com>
2023-12-04 16:17:00 -0300

    Inicialização de projeto em C com menu inicial

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menujogo(){
+    printf("MENU - GAME\n");
+    printf("Insira uma das 3 opções abaixo:\n");
+    printf("1. Iniciar\n2. Carregar Jogo\n3. Sair");
+}
+
+int main(){
+    printf("JOGO DE DOMINÓ");
+
+
+    return 0;
+}
